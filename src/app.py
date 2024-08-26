b89124d5f5b300b3824707d9db74ac5bb8218323
jakob <jakob.g.maier@gmail.com>
2024-08-26 20:56:34 +0200

    work on login

--- src/app.py
+++ src/app.py
@@ -10,6 +10,9 @@ from flask_login import (
     logout_user,
     current_user,
 )
+import bcrypt
+
+import db
 
 app = Flask(__name__)
 app.config["SECRET_KEY"] = "your_secret_key_here"
@@ -37,21 +40,62 @@ class LoginForm(FlaskForm):
     submit = SubmitField("Login")
 
 
+class SignupForm(FlaskForm):
+    username = StringField(
+        "Username", validators=[DataRequired(), Length(min=4, max=25)]
+    )
+    password = PasswordField("Password", validators=[DataRequired(), Length(min=6)])
+    confirm_password = PasswordField(
+        "Confirm Password", validators=[DataRequired(), EqualTo("password")]
+    )
+    submit = SubmitField("Sign Up")
+
+
 @app.route("/login", methods=["GET", "POST"])
 def login():
     form = LoginForm()
     if form.validate_on_submit():
-        # Dummy check for demonstration purposes
         username = form.username.data
         password = form.password.data
-        if username == "jakob" and password == "password":
-            user = User(id=form.username.data)
-            login_user(user)
-            flash("Logged in successfully.", "success")
-            return redirect(url_for("dashboard"))
+
+        user = db.get_user_by_name(username)
+
+        if not user:
+            flash("Unknown username.", "danger")
+
         else:
-            flash("Invalid username or password.", "danger")
+            (id, name, hashed_password) = user
+
+            if bcrypt.checkpw(password.encode("utf-8"), hashed_password):
+                user = User(id=form.username.data)
+                login_user(user)
+                flash("Logged in successfully.", "success")
+                return redirect(url_for("dashboard"))
+            else:
+                flash("Invalid username or password.", "danger")
+
     return render_template("login.html", form=form)
+
+
+@app.route("/signup", methods=["GET", "POST"])
+def signup():
+    form = SignupForm()
+    if form.validate_on_submit():
+        username = form.username.data
+        password = form.password.data
+
+        user = db.get_user_by_name(username)
+
+        if user:
+            flash("Username already exists. Please choose a different one.", "danger")
+        else:
+            salt = bcrypt.gensalt()
+            hashed_password = bcrypt.hashpw(password.encode("utf-8"), salt)
+            db.insert_user(username, hashed_password)
+            flash("You have successfully signed up! Please log in.", "success")
+            return redirect(url_for("login"))
+
+    return render_template("signup.html", form=form)
 
 
 @app.route("/dashboard")
---
+++ src/db.py
@@ -0,0 +1,33 @@
+import sqlite3
+
+database = "/home/pi/clock-in/db/database.sqlite"
+
+
+def insert_user(username, password):
+    con = sqlite3.connect(database)
+    cur = con.cursor()
+    query = "INSERT INTO users (name,password) VALUES (?,?)"
+    cur.execute(query, (username, password))
+    con.commit()
+    con.close()
+
+
+def get_users():
+    con = sqlite3.connect(database)
+    cur = con.cursor()
+    query = "SELECT name, password FROM users"
+    cur.execute(query)
+    users = cur.fetchall()
+    con.close()
+    return users
+
+def get_user_by_name(username):
+    con = sqlite3.connect(database)
+    cur = con.cursor()
+    query = "SELECT id, name, password FROM users WHERE name = ?"
+    cur.execute(query, (username,))
+    user = cur.fetchone()
+    con.commit()
+    con.close()
+    return user
+
