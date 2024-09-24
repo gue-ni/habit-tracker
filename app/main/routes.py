34eca83901c3532b0b973676bab11e3545fdce75
Jakob <jakob.g.maier@gmail.com>
2024-09-24 21:24:18 +0200

    Update routes.py

--- app/main/routes.py
+++ app/main/routes.py
@@ -28,10 +28,9 @@ def get_todos(user_id):
     todo_daily = db.get_todo_daily_events(user_id)
     todo += todo_daily
 
-    todo_week = db.get_todo_weekly_events(user_id)
-
     current_date = db.get_current_date()
-    todo_week = list(filter(lambda e: e[11] != current_date and e[8] < e[7], todo_week))
+    todo_week = db.get_todo_weekly_events(user_id)
+    todo_week = list(filter(lambda e: e[11] != current_date, todo_week))
 
     todo += todo_week
 
@@ -49,7 +48,7 @@ def compute_weekly_streak(dates, current_date, target_weekly_repeat):
 # this should also be called after recording an occurence
 def compute_streak(event_id):
     event = db.get_event(event_id)
-    print(f"compute_streak {event[1]}")
+    # print(f"compute_streak {event[1]}")
     repeat = event[5]
 
     occurences = db.get_all_occurences(event_id=event_id)
@@ -61,7 +60,8 @@ def compute_streak(event_id):
     today = datetime.datetime.now().date()
 
     dates = [
-        datetime.datetime.strptime(occurence[1], "%Y-%m-%d").date() for occurence in occurences
+        datetime.datetime.strptime(occurence[1], "%Y-%m-%d").date()
+        for occurence in occurences
     ]
 
     if dates[0] == today and len(dates) == 1:
@@ -71,7 +71,7 @@ def compute_streak(event_id):
 
     if repeat == "DAILY":
 
-        yesterday = (today - datetime.timedelta(days=1))
+        yesterday = today - datetime.timedelta(days=1)
 
         print(f"today={today}, yesterday={yesterday}")
         print(dates)
@@ -127,25 +127,29 @@ def compute_streak(event_id):
     return streak
 
 
-@bp.route("/dashboard")
-@login_required
-def dashboard():
-    streak_to_recompute = db.get_all_streaks_for_user(current_user.id)
-    print(streak_to_recompute)
+def compute_all_streaks(user_id):
+    streak_to_recompute = db.get_all_streaks_for_user(user_id)
+    # print(streak_to_recompute)
 
     for streak in streak_to_recompute:
         event_id = streak[0]
         old_count = streak[2]
         new_count = compute_streak(event_id=event_id)
         print(f"{streak}, old_count={old_count}, new_count={new_count}")
-        db.update_streak(event_id=event_id, streak=new_count)
+        # db.update_streak(event_id=event_id, streak=new_count)
+
+
+@bp.route("/dashboard")
+@login_required
+def dashboard():
+
+    compute_all_streaks(current_user.id)
 
     all_events = db.get_all_events(current_user.id)
-    # print(f"all_events={all_events}")
 
-    todo = get_todos(current_user.id)
-    print(f"todo={todo}")
+    todos = get_todos(current_user.id)
+    # todos = []
 
     return render_template(
-        "dashboard.html", user=current_user, events_todo=todo, all_events=all_events
+        "dashboard.html", user=current_user, events_todo=todos, all_events=all_events
     )
