45ddb2f6eb8276ae9c72439d7a6b8007e7abaaa3
Jakob <jakob.g.maier@gmail.com>
2024-10-01 20:38:00 +0200

    update to tests

--- tests/test_app.py
+++ tests/test_app.py
@@ -6,13 +6,19 @@ sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
 import pytest
 from app import create_app, db
+from flask import url_for
+
 
 @pytest.fixture()
 def app():
     app = create_app()
-    app.config.update({
-        "TESTING": True,
-    })
+    app.config.update(
+        {
+            "TESTING": True,
+        }
+    )
+
+    app.config["WTF_CSRF_ENABLED"] = False
 
     # other setup can go here
     db.create_db()
@@ -33,29 +39,55 @@ def runner(app):
 
 
 def test_index(client):
-    response = client.get('/')
+    response = client.get("/")
+    assert response.status_code == 200
+
+    response = client.get("/about")
     assert response.status_code == 200
 
 
 def test_signup(client):
-    response = client.post("/user/signup", data={
-        "username": "Max",
-        "password": "MyPassword"
-    })
+    response = client.post(
+        "/user/signup",
+        data={
+            "username": "Max",
+            "password": "MyPassword",
+            "confirm_password": "MyPassword",
+        },
+        follow_redirects=True,
+    )
     assert response.status_code == 200
 
 
-def test_valid_login(client):
-    response = client.post("/user/login", data={
-        "username": "Max",
-        "password": "MyPassword"
-    })
-    assert response.status_code == 200
+def test_login_unknown_user(client):
+    response = client.post(
+        "/user/login",
+        data={"username": "DoesNotExist", "password": "MyPassword"},
+        follow_redirects=True,
+    )
+    assert response.status_code == 404
 
 
-def test_invalid_login(client):
-    response = client.post("/user/login", data={
-        "username": "Max",
-        "password": "Incorrect"
-    })
+def test_login_invalid_password(client):
+    response = client.post(
+        "/user/login",
+        data={"username": "Max", "password": "WrongPassword"},
+        follow_redirects=True,
+    )
     assert response.status_code == 400
+
+
+def test_login_valid_password(client):
+    response = client.post(
+        "/user/login",
+        data={"username": "Max", "password": "MyPassword"},
+        follow_redirects=True,
+    )
+    assert response.status_code == 200
+    assert len(response.history) == 1
+    assert response.request.path == "/dashboard"
+
+    response = client.get("/user/logout", follow_redirects=True)
+    assert response.status_code == 200
+    assert len(response.history) == 1
+    assert response.request.path == "/"
