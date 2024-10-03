b8b57d9357a15c80bda446f61614122bb2253292
Jakob <45669207+gue-ni@users.noreply.github.com>
2024-10-03 09:09:08 +0200

    add new tests

---
+++ tests/test_utils.py
@@ -0,0 +1,14 @@
+import sys
+import os
+import pytest
+
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+from app import utils
+
+
+def test_todo():
+    pass 
+
+
+def test_streaks():
+    pass
